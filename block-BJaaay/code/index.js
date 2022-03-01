1dcf400ca9e1ad2a4f00f6e5499459dd74126673
rahul raushan <raushanrahul23@gmail.com>
2022-03-01 07:22:48 +0530

    update-1

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -6,8 +6,33 @@
 
 // Using `console.log` log all the evan values from 1 to 10.
 
-// Using `console.log` log all the od values from 1 to 10.
+// Using `console.log` log all the odd values from 1 to 10.
 
 // Calculate the sum of all numbers from 1 to 10.
 
 // Log all the values from 1 to 10 using while loop
+
+for(let i = 1; i <= 10; i = i + 1){
+    console.log(i);
+}
+
+//
+
+for(let i = 1; i  <= 10; i = i+1 ){
+    if(i % 2 == 0)
+    console.log(i);
+}
+
+// 
+
+for(let i = 1; i <= 10; i = i+1){
+    if(i % 2 !== 0 )
+    console.log(i);
+}
+
+//
+let i = 1;
+while(i <= 10){
+    console.log(i)
+    i = i+ 1;
+}
